683e20442767534de85a946f3f24447d4f41b341
Lucas Mbunga António Pascoal <bernivaldolucaspascoal@gmail.com>
2026-03-25 21:35:04 +0100

    git commit -m"C"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import queue.Queue;
 import stack.Stack;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //Testes da estrutura de Pilha
 
         Stack<String> stack=new Stack<>();
         stack.push("Lucas");
@@ -21,5 +24,19 @@ public class Main {
         System.out.println(stack.toString());
         System.out.println("Peek: "+stack.peek());
         System.out.println("Size: "+stack.size());
+
+        //Testes da estrutura de Fila
+
+        Queue<String> queue=new Queue<>();
+        queue.enqueue("Lucas");
+        queue.enqueue("Maria");
+        queue.enqueue("Antonio");
+
+        System.out.println("-------------------------FILA--------------------------");
+        System.out.println(queue.toString());
+
+        queue.dequeue();
+        System.out.println(queue.toString());
+
     }
 }
